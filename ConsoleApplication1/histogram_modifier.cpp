eb0fd1b908264c09c78513f38751eefacac0de3c
BUPE <emirebp@gmail.com>
2020-02-02 18:40:48 -0600

    added comments, and normalize variables

--- ConsoleApplication1/histogram_modifier.cpp
+++ ConsoleApplication1/histogram_modifier.cpp
@@ -9,68 +9,71 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-    Mat src, dst;
+    //Creamos las variables necesarias
+    Mat src, histogramResult, imageEqual;
 
-    /// Load image
+    // Se carga la imagen desde la carpeta donde se encuentra el programa
     src = imread("../../histogram_modifier/histogram_test.jpg", IMREAD_GRAYSCALE);
-
+    //Se observa si  la imagen existe, si no, no se realiza nada
     if (!src.data)
     {
         return -1;
     }
 
-    /// Separate the image in 3 places ( B, G and R )
-    vector<Mat> bgr_planes;
-    split(src, bgr_planes);
+    // Establecemos el numero de bins
+    int numberhist = 255;
+    // Valores para el dibujo del histograma
+    //Se establece el alto del histograma
+    int altoHist = 256; 
+    //Se estbalece el largo del histograma
+    int anchoHist = 400;
+    //Se establece el peso de los bins, en este caso es 1
+    int pesohist = 1;
 
-    /// Establish the number of bins
-    int histSize = 255;
+    //Seteamos los rangos de nuestro histograma
+    float rango[] = { 0, 256 };
+    const float* rangoHist = { rango };
+    //Seteamos los rangos de iamgen recuperada, con relacion al histograma. Se basan en el largo y alto
+    //ya puestos, anteriormente
+    float secondRango[] = { 160, 400 };
+    const float* rangoEqual = { secondRango };
 
-    /// Set the ranges ( for B,G,R) )
-    float range[] = { 0, 256 };
-    const float* histRange = { range };
-    float newRange[] = { 180, 256 };
-    const float* BackRange = { newRange };
+    //Se caclcula el histograma
+    calcHist(&src, 1, 0, Mat(), histogramResult, 1, &numberhist, &rangoHist, true, false);
+    //Se creaca una imagen con los cosntructores de:
+    //Rows:400
+    //cols:256
+    //Type: tipo de imagen en este caso es una imagen de un solo canal, por lo tanto se pone 
+    //CV_80C1: que significa imagen de 8 bits de un canal
+    //Scalar: un escalar en este caso 0
+    Mat graphicHist(anchoHist, altoHist, CV_8UC1, Scalar(0, 0, 0));
 
-    bool uniform = true; bool accumulate = false;
+    ///Se normalizan los resultados, de el valor maximo que pusimos en este caso 400, a un valor normal 
+    //de 256, esto incrementala informacion de nuestro algoritmo
+    normalize(histogramResult, histogramResult, 0, graphicHist.rows, NORM_MINMAX, -1, Mat());
 
-    Mat histogramResult;
-
-    /// Compute the histograms:
-    calcHist(&src, 1, 0, Mat(), histogramResult, 1, &histSize, &histRange, uniform, accumulate);
-
-
-    // Draw the histograms for B, G and R
-    int hist_w = 512; int hist_h = 400;
-    int bin_w = cvRound((double)hist_w / histSize);
-
-    Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
-
-    /// Normalize the result to [ 0, histImage.rows ]
-    normalize(histogramResult, histogramResult, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-
-
-    /// Draw for each channel
-    for (int i = 1; i < histSize; i++)
+    //Se dibuja una linea siguiendo los datos de nuestro algoritmo, para crear nuestra grafica
+    for (int i = 1; i < numberhist; i++)
     {
-        line(histImage,
-            Point(bin_w * (i - 1), 
-            hist_h - cvRound(histogramResult.at<float>(i - 1))),
-            Point(bin_w * (i), 
-            hist_h - cvRound(histogramResult.at<float>(i))),
+        line(graphicHist,
+            Point(pesohist * (i - 1), 
+            anchoHist - cvRound(histogramResult.at<float>(i - 1))),
+            Point(pesohist * (i), 
+            anchoHist - cvRound(histogramResult.at<float>(i))),
             Scalar(255, 0, 0), 2, 8, 0);
 
     }
-
-    Mat backproj;
-    calcBackProject(&src, 1, 0, histogramResult, backproj, &BackRange, 1, true);
-
-    /// Display
+    //Calculamos en base anuestro histogram y el rango que deseamos la nueva imagen
+    calcBackProject(&src, 1, 0, histogramResult, imageEqual, &rangoEqual, 1, true);
+    //Agreamos un texto a la imagen 
+    putText(imageEqual, "Rango de 160 a 400", Point(50,100), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 255, 255));
+    putText(src, "Dos piezas de lego naranja y amarilla", Point(50, 100), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 255, 255));
+    //Mostramos las iamgenes
     namedWindow("calcHist Demo", WINDOW_AUTOSIZE);
-    imshow("calcHist Demo", histImage);
-    imshow("Redraw", backproj);
+    imshow("calcHist Demo", graphicHist);
+    imshow("Redraw", imageEqual);
     imshow(" Demo", src);
-    waitKey(0);
 
+    while (char(waitKey(1)) != 27) {}
     return 0;
 }
